00b53d1c9de3cdaad696024f52ff94e5d1a954be
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2023-11-21 18:39:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+var element = document.getElementById('level');
+
+  // Calculate the DOM level
+  var domLevel = 0;
+  var currentElement = element;
+  while (currentElement.parentNode) {
+    domLevel++;
+    currentElement = currentElement.parentNode;
+  }
+
+
+  // Display the final answer using alert()
+  alert('The level of the element is: ' + domLevel);
